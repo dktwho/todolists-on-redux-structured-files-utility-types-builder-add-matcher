330987a900563e05c1c79be86292f5d7c897eeba
Alexander Korobov <dktwho@mail.ru>
2023-11-14 17:37:54 +0300

    structure file: destructuring props in FilterTasksButton

--- src/features/TodolistsList/ui/Todolist/FilterTasksButton/FilterTasksButton.tsx
+++ src/features/TodolistsList/ui/Todolist/FilterTasksButton/FilterTasksButton.tsx
@@ -7,35 +7,37 @@ type Props = {
     todolist: TodolistDomainType
 }
 export const FilterTasksButton = ({todolist}: Props) => {
+
+    const {id, filter} = todolist
     const {changeTodolistFilter} = useActions(todolistsActions);
     const onAllClickHandler = () => {
-        changeTodolistFilter({filter: "all", id: todolist.id})
+        changeTodolistFilter({filter: "all", id})
     }
     const onActiveClickHandler = () => {
-        changeTodolistFilter({filter: "active", id: todolist.id})
+        changeTodolistFilter({filter: "active", id})
     }
     const onCompletedClickHandler = () => {
-        changeTodolistFilter({filter: "completed", id: todolist.id})
+        changeTodolistFilter({filter: "completed", id})
     }
 
     return (
         <>
             <Button
-                variant={todolist.filter === "all" ? "outlined" : "text"}
+                variant={filter === "all" ? "outlined" : "text"}
                 onClick={onAllClickHandler}
                 color={"inherit"}
             >
                 All
             </Button>
             <Button
-                variant={todolist.filter === "active" ? "outlined" : "text"}
+                variant={filter === "active" ? "outlined" : "text"}
                 onClick={onActiveClickHandler}
                 color={"primary"}
             >
                 Active
             </Button>
             <Button
-                variant={todolist.filter === "completed" ? "outlined" : "text"}
+                variant={filter === "completed" ? "outlined" : "text"}
                 onClick={onCompletedClickHandler}
                 color={"secondary"}
             >
