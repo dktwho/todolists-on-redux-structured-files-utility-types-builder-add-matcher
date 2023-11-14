f777b9aee47715a859f9207de089b7ad2526bd6a
Alexander Korobov <dktwho@mail.ru>
2023-11-14 14:59:02 +0300

    type: tasks.reducer modified type with Record

--- src/features/TodolistsList/tasks.reducer.ts
+++ src/features/TodolistsList/tasks.reducer.ts
@@ -144,6 +144,4 @@ export type UpdateDomainTaskModelType = {
   deadline?: string;
 };
 
-export type TasksStateType = {
-  [key: string]: Array<TaskType>;
-};
+export type TasksStateType = Record<string, TaskType[]>
