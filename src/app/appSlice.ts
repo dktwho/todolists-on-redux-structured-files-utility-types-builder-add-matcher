a2fcd2cb8565c4934e6c15a22b707de179fec3fa
Alexander Korobov <dktwho@mail.ru>
2023-11-15 09:44:16 +0300

    feat: add to addMatcher, for rejected and fulfilled

--- src/app/appSlice.ts
+++ src/app/appSlice.ts
@@ -29,6 +29,16 @@ const slice = createSlice({
         }, (state, action) => {
             state.status = 'loading'
         })
+        builder.addMatcher((action: AnyAction) => {
+            return action.type.endsWith('/rejected')
+        }, (state, action) => {
+            state.status = 'failed'
+        })
+        builder.addMatcher((action: AnyAction) => {
+            return action.type.endsWith('/fulfilled')
+        }, (state, action) => {
+            state.status = 'succeeded'
+        })
     }
 });
 
