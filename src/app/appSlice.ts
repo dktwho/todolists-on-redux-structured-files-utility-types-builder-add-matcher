2f08f6d6a40504bf0d43a3b7c66827eaf0230f6d
Alexander Korobov <dktwho@mail.ru>
2023-11-15 09:39:29 +0300

    feat: add extraReducers to appSLice, addMatcher for status = loading, comment utils thunkTryCatch dispatch setAppStatus

--- src/app/appSlice.ts
+++ src/app/appSlice.ts
@@ -1,28 +1,35 @@
-import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import {AnyAction, createSlice, PayloadAction} from "@reduxjs/toolkit";
 
 const initialState = {
-  status: "idle" as RequestStatusType,
-  error: null as string | null,
-  isInitialized: false,
+    status: "idle" as RequestStatusType,
+    error: null as string | null,
+    isInitialized: false,
 };
 
 export type AppInitialStateType = typeof initialState;
 export type RequestStatusType = "idle" | "loading" | "succeeded" | "failed";
 
 const slice = createSlice({
-  name: "app",
-  initialState,
-  reducers: {
-    setAppError: (state, action: PayloadAction<{ error: string | null }>) => {
-      state.error = action.payload.error;
+    name: "app",
+    initialState,
+    reducers: {
+        setAppError: (state, action: PayloadAction<{ error: string | null }>) => {
+            state.error = action.payload.error;
+        },
+        setAppStatus: (state, action: PayloadAction<{ status: RequestStatusType }>) => {
+            state.status = action.payload.status;
+        },
+        setAppInitialized: (state, action: PayloadAction<{ isInitialized: boolean }>) => {
+            state.isInitialized = action.payload.isInitialized;
+        },
     },
-    setAppStatus: (state, action: PayloadAction<{ status: RequestStatusType }>) => {
-      state.status = action.payload.status;
-    },
-    setAppInitialized: (state, action: PayloadAction<{ isInitialized: boolean }>) => {
-      state.isInitialized = action.payload.isInitialized;
-    },
-  },
+    extraReducers: (builder) => {
+        builder.addMatcher((action: AnyAction) => {
+            return action.type.endsWith('/pending')
+        }, (state, action) => {
+            state.status = 'loading'
+        })
+    }
 });
 
 export const appReducer = slice.reducer;
