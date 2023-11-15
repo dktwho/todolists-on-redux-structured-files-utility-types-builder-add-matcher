643df406704f0a9a2c573b5d99b6d709cf0b7eaf
Alexander Korobov <dktwho@mail.ru>
2023-11-15 09:49:22 +0300

    feat: authSlice change all addCase to addMatcher with logic for every action.type

--- src/common/utils/thunk-try-catch.ts
+++ src/common/utils/thunk-try-catch.ts
@@ -1,8 +1,7 @@
-import { AppDispatch, AppRootStateType } from "app/store";
-import { handleServerNetworkError } from "common/utils/handle-server-network-error";
-import { BaseThunkAPI } from "@reduxjs/toolkit/dist/createAsyncThunk";
-import { appActions } from "app/appSlice";
-import { BaseResponseType } from "common/types";
+import {AppDispatch, AppRootStateType} from "app/store";
+import {handleServerNetworkError} from "common/utils/handle-server-network-error";
+import {BaseThunkAPI} from "@reduxjs/toolkit/dist/createAsyncThunk";
+import {BaseResponseType} from "common/types";
 
 export const thunkTryCatch = async <T>(
   thunkAPI: BaseThunkAPI<AppRootStateType, unknown, AppDispatch, null | BaseResponseType>,
